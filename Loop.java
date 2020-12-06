a9a40b7def54d926288490ba73a6dd44de090d27
Tabasco Au <aulokkin222@hotmail.com>
2020-12-06 14:01:11 +0800

    waiyu stuffs

--- Loop.java
+++ Loop.java
@@ -3,19 +3,30 @@ import java.util.logging.Logger;
 
 class HelloWorld {
     public static void main(String[] args) {
-        int rowSize = 10;
-        int columnSize = 8;
-        int tileSize = 5;
-        for (int row = 0; row < rowSize; row++) {
-            for (int column = 0; column < columnSize; column++) {
-                if (row > 0) {
-                    
-                } else {
-                    System.out.print("#");
-                }
-            }
-           
+        int array1[] = {1,2,1};
+        int array2[] = {4,3};
+        String operator = "*";
+        String answer1 = "";
+        String answer2 = "";
+        int finalAnswer = 0;
+
+        for (int i=0; i<array1.length; i++) {
+            answer1 += String.valueOf(array1[i]);
+        }
+        for (int i=0; i<array2.length;  i++)  {
+            answer2 += String.valueOf(array2[i]);
         }
 
+        if (operator == "+") {
+            finalAnswer = Integer.parseInt(answer1) + Integer.parseInt(answer2);
+        } else if (operator == "-") {
+            finalAnswer = Integer.parseInt(answer1) - Integer.parseInt(answer2);
+        } else if (operator == "*") {
+            finalAnswer = Integer.parseInt(answer1) * Integer.parseInt(answer2);
+        }
+        System.out.println("finalAnswer: " + finalAnswer);
+       
     }
+
+    
 }
